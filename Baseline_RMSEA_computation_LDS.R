844ed5944fa32b8564a3c5cd296564e063a612e6
Connor McCabe <cmccabe@uw.edu>
2021-03-01 16:59:59 -0800

    Add files via upload

---
+++ Baseline_RMSEA_computation_LDS.R
@@ -0,0 +1,37 @@
+# Minor setup
+packages.req <- c("semTools","dplyr")
+new.packages <- packages.req[!(packages.req %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+require(dplyr)
+require(semTools)
+#Below read in the RDS file that contains all available fits from the lavaan object.
+#These fits are then coverted to a list for easy reading.
+fits.lds<-readRDS(url("https://github.com/connorjmccabe/Dev_Imbalance_Code/blob/master/DI_LDS_fits.rds?raw=true"))%>% as.list()
+
+
+##the object fits.lds provides 72 fit measures that readers are welcomed/encouraged
+# to examine. We report several in our manuscript, but describe below a consideration
+#for interpreting CFIs.
+
+#Below, we compute scaled RMSEA value manually using the scaled variant given the use of
+#MLR in analyses. We compare a manual computation to the estimated one as a "check"
+#before computing this value for the baseline model.
+
+sqrt(fits.lds$chisq.scaled-fits.lds$df.scaled)/sqrt(fits.lds$df.scaled*(fits.lds$ntotal-1))
+fits.lds$rmsea.scaled#We can see these are essentially identical.
+
+#Using the same formula, we can now compute this value for the baseline (null) model:
+(fits.lds$rmsea.baseline.scaled<-sqrt(fits.lds$baseline.chisq.scaled-fits.lds$baseline.df.scaled)/sqrt(fits.lds$baseline.df.scaled*(fits.lds$ntotal-1)))
+
+#The baseline RMSEA value was 0.1078, which is below the recommended RMSEA value
+#of 0.15.
+
+#As a more formal final check, we used the nullRMSEA function from semTools
+#to compute this value. When supplied with our estimated model, results
+#were as follows:
+
+#> nullRMSEA(lds.final,scaled=T)
+#The baseline model's RMSEA = 0.1112
+
+#CFI, TLI, and other incremental fit indices may not be very informative because the baseline model's RMSEA < 0.158 (Kenny, Kaniskan, & McCoach, 2015).
+
